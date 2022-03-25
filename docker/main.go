c81bde4b93c01ff315f24b7ec2d16ced116eca5e
Jason mann <793650314@qq.com>
2022-03-25 17:28:02 +0800

    docker

--- docker/main.go
+++ docker/main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
+
+var db *sql.DB
+
+type user struct {
+	id   int
+	age  int
+	name string
+}
+
+func initDB() (err error) {
+	dsn := "root:secret@tcp(mysql:3306)/homestead"
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	// 尝试与数据库建立连接（校验dsn是否正确）
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// 查询单条数据示例
+func queryRowDemo() user {
+	sqlStr := "select id, name, age from users where id=?"
+	var u user
+	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+	err := db.QueryRow(sqlStr, 1).Scan(&u.id, &u.name, &u.age)
+	if err != nil {
+		fmt.Printf("scan failed, err:%v\n", err)
+	}
+
+	return u
+}
 
 func main() {
 	http.HandleFunc("/", hello)
 	server := &http.Server{
 		Addr: ":8888",
 	}
-	fmt.Println("server startup...")
+	fmt.Println("server startup...",db)
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Printf("server startup failed, err:%v\n", err)
 	}
 }
 
 func hello(w http.ResponseWriter, _ *http.Request) {
-	w.Write([]byte("hello jason.com!"))
+	fmt.Println("lllllll")
+	w.Write([]byte("hello jason.com! "))
 }
